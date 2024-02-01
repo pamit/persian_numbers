d7f64ce01b2d1af4abe61918cead7e2b414a57bf
Payam Mousavi <5113276+pamit@users.noreply.github.com>
2024-02-01 16:22:23 +1100

    Update persian_numbers_spec.rb

--- spec/persian_numbers_spec.rb
+++ spec/persian_numbers_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe PersianNumbers do
   describe '.translate_to_words' do
     context 'Testing GHA failed build scenario' do
+      it { expect(true).to eq(true) }
       it { expect(true).to eq(false) }
     end
 
